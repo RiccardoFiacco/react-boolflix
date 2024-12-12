c819a0b972e76b486d2ffefaee5a4e20d368d08a
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-12 18:52:50 +0100

    changes on app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Header } from './components/Header.jsx';
 import { Main } from './components/Main.jsx';
 import {uriTvBase, uriMovieBase, uriFilterMovieBase, uriFilterTvBase} from './util.js'
 
-function App() {
 
+function App() {
   return (
     <GlobalContext.Provider value={{uriMovieBase, uriTvBase, uriFilterMovieBase, uriFilterTvBase}}>{/*usiamo un context per rendere globali a tutti i componenti quei valori*/}
         <Header/>
