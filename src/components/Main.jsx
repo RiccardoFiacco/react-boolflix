272de7f42418d00bf2d8238ec0dce2c28ec6522c
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-12 18:54:15 +0100

    changes on main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,62 +1,71 @@
-import { axiosSetCall, changeHandler,searchFunction } from '../util.js';
-import { useEffect, useState, useContext} from 'react';
-import { GlobalContext } from '../GlobalContext.js';
-import { Card } from './Card.jsx';
+import { axiosSetCall, changeHandler, searchFunction } from "../util.js";
+import { useEffect, useState, useContext } from "react";
+import { GlobalContext } from "../GlobalContext.js";
+import { Card } from "./Card.jsx";
+import useMovies from "../hooks/useMovies.jsx";
+export function Main() {
+  const { uriTvBase, uriMovieBase, uriFilterMovieBase, uriFilterTvBase } =
+    useContext(GlobalContext);
 
-export function Main(){
+  const [uriTv, setUriTv] = useState(uriTvBase);
+  const [uriMovie, setUriMovie] = useState(uriMovieBase);
 
-    const {uriTvBase, uriMovieBase, uriFilterMovieBase, uriFilterTvBase} = useContext(GlobalContext)
+  const [searchInput, setSearchInput] = useState(""); //creo una variabile reattiva per andare a gestire il valore di input
+  const [movies, setMovies] = useState([]); //creo una variabile reattiva per andare a salvare gli oggetti film
+  const [tvSeries, setTvSeries] = useState([]); //creo una variabile reattiva per andare a salvare gli oggetti serie
 
-    const [uriTv, setUriTv] = useState(uriTvBase);
-    const [uriMovie, setUriMovie] = useState(uriMovieBase);
+//   const [movies2, searchMovie2] = useMovies();
+    
+  useEffect(() => {
+    //use effect che al montaggio del componente invoca queste due funzioni
+    axiosSetCall(uriMovie, setMovies); //questa recupera e aggiorna la variabile movies
+    axiosSetCall(uriTv, setTvSeries); //questa recupera e aggiorna la variabile tvSeries
+    
+  }, [uriMovie, uriTv]); //dipendenza al cambio dell' url
 
-    const [searchInput, setSearchInput] = useState('') //creo una variabile reattiva per andare a gestire il valore di input
-    const [movies, setMovies] = useState([]) //creo una variabile reattiva per andare a salvare gli oggetti film
-    const [tvSeries, setTvSeries] = useState([]) //creo una variabile reattiva per andare a salvare gli oggetti serie
+  function onSubmit() {
+    const query = searchFunction(searchInput);
+    setUriTv(uriFilterTvBase + query);
+    setUriMovie(uriFilterMovieBase + query);
+  }
 
-    //const [movies2, refetchMovies2] = useMovies();
+  function reset() {
+    setUriTv(uriTvBase);
+    setUriMovie(uriMovieBase);
+  }
 
-    useEffect(()=>{ //use effect che al montaggio del componente invoca queste due funzioni
-      axiosSetCall(uriMovie, setMovies) //questa recupera e aggiorna la variabile movies
-      axiosSetCall(uriTv, setTvSeries)//questa recupera e aggiorna la variabile tvSeries
-    },[uriMovie, uriTv])//dipendenza al cambio dell' url
+  return (
+    <main>
+      {/* input che al change chiama una funzione che modifica il valore searchInput e con valore uguale a searchInput*/}
+      <input type="text" onChange={(e) => changeHandler(e, setSearchInput)} name="title" value={searchInput} ></input>
+      {/*button che al click esegue la funzione search function e l'altra resetta tutto*/}
+      <button onClick={onSubmit}>invio</button>
+      <button onClick={reset}>reset</button>
+      {/* <h3>prova fetch</h3>
+      <div>{movies2.map((movie, i) => {
+              return <p key={i}> {movie.title}</p>;
+            })}</div> */}
+      <h3>Film</h3>
+      <div>
+        {
+          //map per creare dei paragrafi con i titoli dei film
+          movies &&
+            movies.map((movie, i) => {
+              return <Card obj={movie} key={i} />;
+            })
+        }
+      </div>
 
-    function onSubmit(){
-      const query = searchFunction(searchInput)
-      setUriTv(uriFilterTvBase + query);
-      setUriMovie(uriFilterMovieBase + query);
-    }
-
-    function reset(){
-      setUriTv(uriTvBase);
-      setUriMovie(uriMovieBase);
-    }
-
-    return(
-        <main>
-            {/* input che al change chiama una funzione che modifica il valore searchInput e con valore uguale a searchInput*/}
-            <input type='text' onChange={(e) => changeHandler(e, setSearchInput)} name="title" value={searchInput}></input> 
-            
-            <button onClick={onSubmit}>invio</button>{/*button che al click esegue la funzione search function*/}
-            <button onClick={reset}>reset</button>
-                
-            <h3>Film</h3>
-            <div>{//map per creare dei paragrafi con i titoli dei film
-            movies && movies.map((movie, i)=>{
-                return(
-                    <Card obj={movie}  key={i}/>
-                )
-             })  
-            }</div>
-        
-            <h3>Serie tv</h3>
-            <div>{//map per creare dei paragrafi con i titoli delle serie
-            tvSeries && tvSeries.map((serie, i)=>{
-                return(
-                    <Card obj={serie}  key={i}/>
-                )
-             })  
-            }</div>
-        </main>
-    )
+      <h3>Serie tv</h3>
+      <div>
+        {
+          //map per creare dei paragrafi con i titoli delle serie
+          tvSeries &&
+            tvSeries.map((serie, i) => {
+              return <Card obj={serie} key={i} />;
+            })
+        }
+      </div>
+    </main>
+  );
 }
