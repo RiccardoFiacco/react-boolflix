e9be0f54a3c4559a6c2f8ede9a758df34e29cabb
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-12 23:14:47 +0100

    change the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,18 @@ import { flagImage } from "../util";
 export function Card({obj={}}){
 
     const {original_language, vote_average} = obj;
-    let img = flagImage(original_language);
+    let language = flagImage(original_language);
     
     return(
-        <ol>
-            {obj.title ?? obj.name ?? 'Titolo Assente'}
-            <li>{obj.original_title ?? obj.original_name}</li>
-            <li>{
-                img ? <img src={img} height="25px" alt=""/> : original_language
+        <>
+        <div>{obj.title ?? obj.name ?? 'Titolo Assente'}</div>
+        <ol>    
+            <li>original name: {obj.original_title ?? obj.original_name}</li>
+            <li>language: {
+                language ? <img src={language} height="25px" alt=""/> : original_language
             }</li>
-            <li>{vote_average}</li>
+            <li>avarage vote: {vote_average}</li>
         </ol>
+        </>
     )
 }
