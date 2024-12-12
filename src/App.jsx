b763b2fb8818a809b7105257d8c19b00a9918a6d
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-12 12:15:33 +0100

    ad base for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import './App.css';
+import { GlobalContext } from './GlobalContext.js';
+
 
 function App() {
 
+  const uri_tv = "https://api.themoviedb.org/3/discover/tv?api_key=3a55960cb8d2cc735fc2a215dc42af3e&";
+  const uri_movie = "https://api.themoviedb.org/3/discover/movie?api_key=3a55960cb8d2cc735fc2a215dc42af3e&";
+
   return (
-    <>
-      
-    </>
+    <GlobalContext.Provider value={{uri_movie, uri_tv}}>
+      <h1>Boolflix</h1>
+    </GlobalContext.Provider>
   )
 }
 
