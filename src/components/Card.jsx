6fbe1c000fe9b720b183b804e6c7284f4592ef25
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-12 18:53:28 +0100

    create card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,16 @@
+import { flagImage } from "../util";
+
 export function Card({obj={}}){
+
     const {original_language, vote_average} = obj;
-    let img;
-    function flag(original_language){
-        switch(original_language){
-            case "en" :
-                return img= "https://flagpedia.net/data/flags/w580/gb.webp"      
-            case "it":
-                return img = "https://flagpedia.net/data/flags/w580/it.webp"    
-            case "de":
-                return img = "https://flagpedia.net/data/flags/w580/de.webp"    
-            case "es":
-                return img = "https://flagpedia.net/data/flags/w580/es.webp"   
-            default: 
-                return 
-        }
-    }
+    let img = flagImage(original_language);
     
     return(
-        <ol>{obj.title? obj.title : obj.name}
-            <li>{obj.original_title ? obj.original_title : obj.original_name}</li>
+        <ol>
+            {obj.title ?? obj.name ?? 'Titolo Assente'}
+            <li>{obj.original_title ?? obj.original_name}</li>
             <li>{
-                flag(original_language)? <img src={img} height="25px" alt=""/> : original_language
+                img ? <img src={img} height="25px" alt=""/> : original_language
             }</li>
             <li>{vote_average}</li>
         </ol>
