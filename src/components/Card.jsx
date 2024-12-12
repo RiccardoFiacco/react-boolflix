3bcffa890a250964ed9f7356a148492057ac5652
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-13 00:10:43 +0100

    add star as a value

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,14 @@
 import { flagImage, imagePath } from "../util";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { getStar } from "../util";
 
 export function Card({obj={}}){
 
     const {original_language, vote_average} = obj;
     let language = flagImage(original_language);
-    
+
+    let star = getStar(vote_average);
+
     return(
         <>
         <div>{obj.title ?? obj.name ?? 'Titolo Assente'}</div>
@@ -14,7 +18,11 @@ export function Card({obj={}}){
             <li>language: {
                 language ? <img src={language} height="25px" alt=""/> : original_language
             }</li>
-            <li>avarage vote: {vote_average}</li>
+            <li>avarage vote: {star}</li>
+
+
+
+            {/* <FontAwesomeIcon icon="fa-solid fa-star" /> */}
         </ol>
         </>
     )
