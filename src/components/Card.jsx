d75f2df586adfefe1ceb9b07868cff46ed8fec16
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-12 17:15:49 +0100

    add the flag for the lenguage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,27 @@
 export function Card({obj={}}){
     const {original_language, vote_average} = obj;
+    let img;
+    function flag(original_language){
+        switch(original_language){
+            case "en" :
+                return img= "https://flagpedia.net/data/flags/w580/gb.webp"      
+            case "it":
+                return img = "https://flagpedia.net/data/flags/w580/it.webp"    
+            case "de":
+                return img = "https://flagpedia.net/data/flags/w580/de.webp"    
+            case "es":
+                return img = "https://flagpedia.net/data/flags/w580/es.webp"   
+            default: 
+                return 
+        }
+    }
+    
     return(
         <ol>{obj.title? obj.title : obj.name}
             <li>{obj.original_title ? obj.original_title : obj.original_name}</li>
-            <li>{original_language}</li>
+            <li>{
+                flag(original_language)? <img src={img} height="25px" alt=""/> : original_language
+            }</li>
             <li>{vote_average}</li>
         </ol>
     )
