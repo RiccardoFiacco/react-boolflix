3f5065f5f89aaa8cdbefd7b273e6a8796523f363
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-13 12:07:38 +0100

    add style to the card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,26 +5,27 @@ import { Card } from "./Card.jsx";
 export function Main() {
 
   let {movies, tvSeries} = useContext(GlobalContext) 
+
   return (
     <main className="container">
-      <div className="row">
+      <div className="row row-gap-3">
         <h2>Film</h2>
         {//map per creare dei paragrafi con i titoli dei film
             movies && movies.map((movie, i) => {
                 return (
-                  <div className="col"key={i} >
+                  <div className="col-8 col-md-6 col-lg-3 "key={i} >
                     <Card obj={movie} />
                   </div>)   
               })
         }  
       </div>
 
-      <div className="row">
+      <div className="row row-gap-3">
         <h2>Serie tv</h2>
           {//map per creare dei paragrafi con i titoli delle serie
             tvSeries && tvSeries.map((serie, i) => {
                 return (
-                <div className="col"key={i} >
+                <div className="col-8 col-md-6 col-lg-3 "key={i} >
                   <Card obj={serie} />
                 </div>);
               })
