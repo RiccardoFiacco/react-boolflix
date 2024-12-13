9ba1fa4d77263d760f1f3e7d02a33d5c8889cf9c
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-13 17:57:12 +0100

    add comment

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,38 +6,12 @@ export function Card({ obj = {} }) {
 
   const { title, original_title, original_language, vote_average } = obj; //destructuring 
 
-  let language = flagImage(original_language);
+  let language = flagImage(original_language); //prende l'immagine della bandiera
 
-  let star = getStar(vote_average);
-  let finalImg = imagePath + obj.poster_path;
+  let star = getStar(vote_average); //genera le stelle della valutazione
+  let finalImg = imagePath + obj.poster_path; //setta l'url per recuperare l'immagine
 
  return (
-//     <div>
-//       {/* Card Content */}
-//       <div
-//         style={{ backgroundImage: `url(${finalimag})` }}
-//         className="myCard"
-//       ></div>
-//       {/* Card Hover */}
-//       <div>
-//         <div className="hidden">
-//           <h3>{title}</h3>
-//           {/* </div><img src={imagePath+obj.poster_path} alt="" /> */}
-//           <ol>
-//             <li>original name: {original_title}</li>
-//             <li>
-//               language:{" "}
-//               {language ? (
-//                 <img src={language} height="25px" alt="" /> /*cambiare l'altezza*/
-//               ) : (
-//                 original_language
-//               )}
-//             </li>
-//             <li>avarage vote: {star}</li>
-//           </ol>
-//         </div>
-//       </div>
-//     </div>
     <div className="card relative">
 
         <img src={obj.poster_path ? finalImg : fotoPlace} className="myCard"/>
