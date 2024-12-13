322545a32fd2416504b56978cc085a9fa58ff285
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-13 10:02:18 +0100

    change main and header to use the filter in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,48 @@
+import { useContext, useState } from "react";
+import { GlobalContext } from "../GlobalContext";
+import { uriFilterMovieBase, uriFilterTvBase, searchFunction, uriTvBase, uriMovieBase } from "../util";
+
+
 export function Header(){
+    let {setUriMovie, setUriTv} = useContext(GlobalContext) 
+
+    const [searchInput, setSearchInput] = useState("");
+
+    function changeHandler(event, callback){ //funzione che viene eseguita al cambiamento del valore di input
+          let value = event.target.value; //andiamo a dare ad una variabile il valore dell'elemento che ha scatenato l'evento
+          callback(value); // aggiorno il valore della variabile reattiva
+          const query = searchFunction(searchInput);
+          setUriTv(uriFilterTvBase + query);
+          setUriMovie(uriFilterMovieBase + query);
+      }
+      function reset() {
+        setUriTv(uriTvBase);
+        setUriMovie(uriMovieBase);
+      }
+
     return(
         <header>
-            <h1>Boolflix</h1>
+            <div className="row pt-4 pb-4">
+                <div className="col">
+                    <h1>Boolflix</h1>
+                </div>
+
+                <div className="col">
+                    {/* input che al change chiama una funzione che modifica il valore searchInput e con valore uguale a searchInput*/}
+                    <input 
+                     type="text" 
+                     onChange={(e) => changeHandler(e, setSearchInput)} 
+                     name="title" 
+                     value={searchInput} 
+                     className=""
+                     ></input>
+                    {/*button che al click esegue la funzione search function e l'altra resetta tutto*/}
+                    {/*<button onClick={onSubmit}>invio</button>*/}
+                    <button 
+                     onClick={reset}
+                     className="btn btn-danger ms-3">reset</button>
+                </div>
+            </div>
         </header>   
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,61 +1,12 @@
-import { axiosSetCall, searchFunction } from "../util.js";
-import { useEffect, useState, useContext } from "react";
+import {  useContext } from "react";
 import { GlobalContext } from "../GlobalContext.js";
 import { Card } from "./Card.jsx";
-import useMovies from "../hooks/useMovies.jsx";
+
 export function Main() {
-  const { uriTvBase, uriMovieBase, uriFilterMovieBase, uriFilterTvBase } =useContext(GlobalContext);
 
-  const [uriTv, setUriTv] = useState(uriTvBase);
-  const [uriMovie, setUriMovie] = useState(uriMovieBase);
-
-  const [searchInput, setSearchInput] = useState(""); //creo una variabile reattiva per andare a gestire il valore di input
-  const [movies, setMovies] = useState([]); //creo una variabile reattiva per andare a salvare gli oggetti film
-  const [tvSeries, setTvSeries] = useState([]); //creo una variabile reattiva per andare a salvare gli oggetti serie
-
-//   const [movies2, searchMovie2] = useMovies();
-    
-  useEffect(() => {
-    //use effect che al montaggio del componente invoca queste due funzioni
-    axiosSetCall(uriMovie, setMovies); //questa recupera e aggiorna la variabile movies
-    axiosSetCall(uriTv, setTvSeries); //questa recupera e aggiorna la variabile tvSeries
-    
-  }, [uriMovie, uriTv]); //dipendenza al cambio dell' url
-
-  // function onSubmit() {
-  //   const query = searchFunction(searchInput);
-  //   setUriTv(uriFilterTvBase + query);
-  //   setUriMovie(uriFilterMovieBase + query);
-  // }
-
-  function changeHandler(event, callback){ //funzione che viene eseguita al cambiamento del valore di input
-      let value = event.target.value; //andiamo a dare ad una variabile il valore dell'elemento che ha scatenato l'evento
-      callback(value); // aggiorno il valore della variabile reattiva
-      const query = searchFunction(searchInput);
-      setUriTv(uriFilterTvBase + query);
-      setUriMovie(uriFilterMovieBase + query);
-  }
-  function reset() {
-    setUriTv(uriTvBase);
-    setUriMovie(uriMovieBase);
-  }
-
+  let {movies, tvSeries} = useContext(GlobalContext) 
   return (
     <main className="container">
-      <div className="row">
-        <div className="col">
-          {/* input che al change chiama una funzione che modifica il valore searchInput e con valore uguale a searchInput*/}
-          <input type="text" onChange={(e) => changeHandler(e, setSearchInput)} name="title" value={searchInput} ></input>
-          {/*button che al click esegue la funzione search function e l'altra resetta tutto*/}
-          {/*<button onClick={onSubmit}>invio</button>*/}
-          <button onClick={reset}>reset</button>
-        </div>
-      </div>
-      
-            {/* <h3>prova fetch</h3>
-            <div>{movies2.map((movie, i) => {
-              return <p key={i}> {movie.title}</p>;
-            })}</div> */}
       <div className="row">
         <h2>Film</h2>
         {//map per creare dei paragrafi con i titoli dei film
