7c615b8c82e95fe793336b22482eb5cb5a064fe1
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-13 16:35:37 +0100

    destructuring

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,41 @@
 import { flagImage, imagePath } from "../util.jsx";
 import { getStar } from "../util.jsx";
 
-export function Card({obj={}}){
+export function Card({ obj = {} }) {
 
-    const {original_language, vote_average} = obj;
-    let language = flagImage(original_language);
+  const { title, original_title, original_language, vote_average } = obj; //destructuring 
 
-    let star = getStar(vote_average);
-    let finalimag = imagePath+obj.poster_path
-    return(
-        <div style={{backgroundImage:`url(${finalimag})`}} className="myCard">
-            <div className="hidden">
-                <h3>{obj.title}</h3>
-                {/* </div><img src={imagePath+obj.poster_path} alt="" /> */}
-                <ol>    
-                    <li>original name: {obj.original_title}</li>
-                    <li>language: {
-                        language ? <img src={language} height="25px" alt=""/> : original_language
-                    }</li>
-                    <li>avarage vote: {star}</li> 
-                </ol>
-            </div>
+  let language = flagImage(original_language);
+
+  let star = getStar(vote_average);
+  let finalimag = imagePath + obj.poster_path;
+
+  return (
+    <div>
+      {/* Card Content */}
+      <div
+        style={{ backgroundImage: `url(${finalimag})` }}
+        className="myCard"
+      ></div>
+      {/* Card Hover */}
+      <div>
+        <div className="hidden">
+          <h3>{title}</h3>
+          {/* </div><img src={imagePath+obj.poster_path} alt="" /> */}
+          <ol>
+            <li>original name: {original_title}</li>
+            <li>
+              language:{" "}
+              {language ? (
+                <img src={language} height="25px" alt="" /> /*cambiare l'altezza*/
+              ) : (
+                original_language
+              )}
+            </li>
+            <li>avarage vote: {star}</li>
+          </ol>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
