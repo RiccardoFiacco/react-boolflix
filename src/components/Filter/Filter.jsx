006a91e42dcb535ecebe962f331e4062383922b2
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-13 10:48:40 +0100

    add filter component

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,45 @@
+import { useState, useContext } from "react";
+import { GlobalContext } from "../../GlobalContext";
+import { uriFilterMovieBase, uriFilterTvBase, searchFunction, uriTvBase, uriMovieBase } from "../../util";
+import  style  from './Filter.module.css'
+
+export function Filter(){
+    let {setUriMovie, setUriTv} = useContext(GlobalContext) 
+    
+    const [searchInput, setSearchInput] = useState("");
+    
+    function changeHandler(event, callback){ //funzione che viene eseguita al cambiamento del valore di input
+        let value = event.target.value; //andiamo a dare ad una variabile il valore dell'elemento che ha scatenato l'evento
+        callback(value); // aggiorno il valore della variabile reattiva
+        const query = searchFunction(searchInput);
+        setUriTv(uriFilterTvBase + query);
+        setUriMovie(uriFilterMovieBase + query);
+    }
+    function reset() {
+        setUriTv(uriTvBase);
+        setUriMovie(uriMovieBase);
+    }
+    return(
+        <div className="col-4">
+            <div className={`row ${style.align_center}`}>
+                <div className="col">
+                    {/* input che al change chiama una funzione che modifica il valore searchInput e con valore uguale a searchInput*/}
+                    <input 
+                    type="text" 
+                    onChange={(e) => changeHandler(e, setSearchInput)} 
+                    name="title" 
+                    value={searchInput} 
+                    className={style.input}
+                    ></input>
+                </div>
+                {/*button che al click esegue la funzione search function e l'altra resetta tutto*/}
+                {/*<button onClick={onSubmit}>invio</button>*/}
+                <div className="col">
+                    <button 
+                    onClick={reset}
+                    className="btn btn-danger ms-3">reset</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,18 @@
-import { useContext, useState } from "react";
-import { GlobalContext } from "../GlobalContext";
-import { uriFilterMovieBase, uriFilterTvBase, searchFunction, uriTvBase, uriMovieBase } from "../util";
-
+import { Filter } from "./Filter/Filter";
 
 export function Header(){
-    let {setUriMovie, setUriTv} = useContext(GlobalContext) 
-
-    const [searchInput, setSearchInput] = useState("");
-
-    function changeHandler(event, callback){ //funzione che viene eseguita al cambiamento del valore di input
-          let value = event.target.value; //andiamo a dare ad una variabile il valore dell'elemento che ha scatenato l'evento
-          callback(value); // aggiorno il valore della variabile reattiva
-          const query = searchFunction(searchInput);
-          setUriTv(uriFilterTvBase + query);
-          setUriMovie(uriFilterMovieBase + query);
-      }
-      function reset() {
-        setUriTv(uriTvBase);
-        setUriMovie(uriMovieBase);
-      }
+    
 
     return(
-        <header>
-            <div className="row pt-4 pb-4">
-                <div className="col">
-                    <h1>Boolflix</h1>
-                </div>
+        <header className="bg-black">
+            <div className="container">
+                <div className="row pb-4 pt-4">
+                    <div className="col">
+                        <h1 className="text-danger">Boolflix</h1>
+                    </div>
 
-                <div className="col">
-                    {/* input che al change chiama una funzione che modifica il valore searchInput e con valore uguale a searchInput*/}
-                    <input 
-                     type="text" 
-                     onChange={(e) => changeHandler(e, setSearchInput)} 
-                     name="title" 
-                     value={searchInput} 
-                     className=""
-                     ></input>
-                    {/*button che al click esegue la funzione search function e l'altra resetta tutto*/}
-                    {/*<button onClick={onSubmit}>invio</button>*/}
-                    <button 
-                     onClick={reset}
-                     className="btn btn-danger ms-3">reset</button>
+                    <Filter/>
+
                 </div>
             </div>
         </header>   
