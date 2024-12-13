70dad5d5675f2862e6cf07dc1b39548e66feb2e2
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-13 16:26:39 +0100

    change style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export function Header(){
 
     return(
         <header className="bg-black">    
-                <div className="d-flex justify-content-between space-pb-4 pt-4 ps-4 pe-4">
+                <div className="d-flex justify-content-between pb-3 pt-3 ps-4">
                     <div className="col-3">
                         <h1 className="text-danger">Boolflix</h1>
                     </div>
