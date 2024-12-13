f9d5d273b3e38b6cc350200a4c180a7242245510
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-14 00:31:36 +0100

    change main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,6 @@
-import {  useContext } from "react";
-import { GlobalContext } from "../GlobalContext.js";
-import { Card } from "./Card/Card.jsx";
 import { List } from "./List.jsx";
 export function Main() {
 
-  let {movies, tvSeries} = useContext(GlobalContext) 
   
   return (
     <main className="container">
