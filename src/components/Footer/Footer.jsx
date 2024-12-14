48892ed8f07ff0a2e1cc57417fdbb55afeba3bdf
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-14 01:15:54 +0100

    add footer and relative style

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../../GlobalContext';
+import { uriTvBase, uriMovieBase } from '../../util';
+import style from './Footer.module.css'
+export function Footer(){
+    let {setUriMovie, setUriTv} = useContext(GlobalContext) 
+    let choseButtonArr = [1,2,3,4,5,6,7,8,9,10]
+
+    function changePage(page){
+        let currentTvPage = uriTvBase + "&page=" + page
+        let currentMoviePage = uriMovieBase + "&page=" + page
+        setUriTv(currentTvPage)
+        setUriMovie(currentMoviePage)
+    }
+
+    return(
+        <div className="bg-dark">
+            <div className="container">
+                <div className="row pt-4 pb-4">
+                    {choseButtonArr.map((number, i)=>{
+                        return(
+                            <div className="col" key={i}> 
+                                <button className={style.myBtn} onClick={()=>changePage(number)}>
+                                    {number}
+                                </button>
+                            </div> 
+                        )
+                    })} 
+                </div>
+            </div> 
+        </div>
+    )
+}
