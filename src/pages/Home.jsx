ef92a81e67f992bc97c01bc8caf1a0cadcce992e
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-14 12:44:51 +0100

    change home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import { Main } from "../components/Main";
+import boolFlixBg from '../assets/netflix-background-gs7hjuwvv2g0e9fj.jpg'
 
 export function Home(){
     return(
-        <div className="bg-dark">
-         <Main/>
+        <div className="home">
+         <div className="row pt-5">
+            <div className="col-6 text-center pt-5 fs-1">
+                Benvenuto nel catalogo Boolflix, scegli tra una vasta selezione di film e serie tv!
+            </div>
+         </div>
         </div>
     )
 }
