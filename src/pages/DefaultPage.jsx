bdb5ca1429b14d19d48f9ab6cec9cddc61183c6c
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-14 12:45:00 +0100

    change default page

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
-import { Footer } from '../components/Footer/Footer';
+
 export function DefaultPage(){
     return(
-        <>
+        <div>
             <Header/>
             <Outlet/>
-            <Footer/>
-        </>   
+        </div>   
     )
 }
