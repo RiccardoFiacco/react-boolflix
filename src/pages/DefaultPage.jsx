0aea9ad2e100e6c14117678b218642ef87a84c96
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-14 01:16:14 +0100

    add footer

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
-
+import { Footer } from '../components/Footer/Footer';
 export function DefaultPage(){
     return(
         <>
             <Header/>
             <Outlet/>
+            <Footer/>
         </>   
     )
 }
