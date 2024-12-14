afef7dd6802456c294a04f8fc20ce9db95ed8856
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-14 15:49:21 +0100

    add comments

--- src/pages/HighestRated.jsx
+++ src/pages/HighestRated.jsx
@@ -5,16 +5,16 @@ import { useContext, useEffect } from "react";
 import { GlobalContext } from "../utils/GlobalContext";
 
 export function HighestRated(){
-    const {setMovies, setTvSeries, setUriTv, setUriMovie} = useContext(GlobalContext) 
-    let movieDesc =  uriMovieBase + "&sort_by=vote_average.desc";
-    let seriesDesc =  uriTvBase + "&sort_by=vote_average.desc";
+    const {setMovies, setTvSeries, setUriTv, setUriMovie} = useContext(GlobalContext)  //importo 
+    let movieDesc =  uriMovieBase + "&sort_by=vote_average.desc"; //creo l'url da cui recuperare i film con valutazione maggiore
+    let seriesDesc =  uriTvBase + "&sort_by=vote_average.desc"; //creo l'url da cui recuperare le serie con valutazione maggiore
 
-    useEffect(() => {//use effect che al montaggio del componente invoca queste due funzioni
+    useEffect(() => {//use effect che al montaggio del componente invoca queste 4 funzioni
         axiosSetCall(movieDesc, setMovies); //questa recupera e aggiorna la variabile movies
         axiosSetCall(seriesDesc, setTvSeries); //questa recupera e aggiorna la variabile tvSeries    
-        setUriMovie(movieDesc)
-        setUriTv(seriesDesc)
-    }, []);
+        setUriMovie(movieDesc) //questa aggiorna il valore dell'uri dei movie
+        setUriTv(seriesDesc)//questa aggiorna il valore dell'uri delle serie 
+    }, []);// al montaggio del componente
 
     return(
         <div className="div bg-dark">
