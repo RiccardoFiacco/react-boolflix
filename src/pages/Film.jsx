e55db866ecc80c8873d7b2671669398e19f0fb3f
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-14 14:45:44 +0100

    add logic that changes list for each page

--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -1,6 +1,18 @@
 import { List } from "../components/List";
-import { Footer } from '../components/Footer/Footer'
+import { Footer } from '../components/Footer/Footer';
+import { useContext, useEffect} from "react";
+import { GlobalContext } from "../utils/GlobalContext";
+import { uriMovieBase, axiosSetCall } from "../utils/util";
+
 export function Film(){
+
+    const {setMovies} = useContext(GlobalContext) 
+
+    useEffect(() => {
+        axiosSetCall(uriMovieBase, setMovies); 
+    }, []);
+
+
     return(
         <div className="div bg-dark">
             <div className="container">
--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,6 +1,17 @@
 import { List } from "../components/List";
-import { Footer } from '../components/Footer/Footer'
+import { Footer } from '../components/Footer/Footer';
+import { useContext, useEffect} from "react";
+import { GlobalContext } from "../utils/GlobalContext";
+import { uriTvBase, axiosSetCall } from "../utils/util";
+
 export function Series(){
+
+    const {setTvSeries} = useContext(GlobalContext) 
+    
+    useEffect(() => {
+        axiosSetCall(uriTvBase, setTvSeries); 
+    }, []);
+
     return(
         <div className="div bg-dark">
             <div className="container">
