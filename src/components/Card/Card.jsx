b62c50e89f2526810595e75a3c70199282cd14dd
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-17 16:54:19 +0100

    removed all cast in the card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,9 @@ import { flagImage, imagePath, getStar, axiosSetCall } from "../../utils/util.js
 import style from './Card.module.css'
 import fotoPlace from '../../assets/elementor-placeholder-image.webp'
 import { NavLink } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { GlobalContext } from "../../utils/GlobalContext.js";
-import axios from "axios";
+
 
 export function Card({ obj = {}, objType, addInList }) {
 
@@ -13,35 +13,17 @@ export function Card({ obj = {}, objType, addInList }) {
   const { title, original_title, original_language, vote_average, id} = obj; //destructuring 
 
   let language = flagImage(original_language); //prende l'immagine della bandiera
-  let type;
-
-  let star = getStar(vote_average); //genera le stelle della valutazione
-  let finalImg = imagePath + obj.poster_path; //setta l'url per recuperare l'immagine
-
-  const [cast, setCast] = useState([])
-
-  objType == 'Serie Tv'? type = 'tv' : type = 'movie';
   
-  useEffect(()=>{
-    axios
-    .get(`https://api.themoviedb.org/3/${type}/${id}/credits?&api_key=3a55960cb8d2cc735fc2a215dc42af3e`)
-    .then((res)=>{
-      console.log(res.data.cast)
-      setCast(res.data.cast)   
-    })
-    .catch((err)=>console.log(err))
-  },[])
-
-   console.log(cast)
-  let actors
-  let app = cast.slice(0, 5)
-    if(Array.isArray(app) ){
-    actors = app.map(actor => ({
-      name: actor.name,
-      character: actor.character,
-    }));
+  let star = getStar(vote_average); //genera le stelle della valutazione
+  
+  let finalImg = imagePath + obj.poster_path; //setta l'url per recuperare l'immagine
+  function setterInfo(){
+    let appObj={
+      ...obj,
+      typeOfObj: objType
+    }
+    setElementInfo(appObj)
   }
-
  return (
     <div className="card relative">
 
@@ -56,15 +38,10 @@ export function Card({ obj = {}, objType, addInList }) {
                {language ?  <img src={language} className={style.h_flag}/> : original_language}
             </p>
             <p>avarage vote: {star}</p>
-            <div>
-               {actors.map(({name, character}, i)=>{
-                return(
-                  <p key={i}>{name} interpreta {character}</p>
-                )
-              })} 
-            </div>
+            
           <button className="btn btn-outline-primary" onClick={addInList}>add in your List</button>
-          <button onClick={()=>setElementInfo(obj)}><NavLink to={`/${objType}/${id}`} state={{el:obj}}>Dettagli</NavLink></button>
+          <button onClick={()=>setterInfo()}><NavLink to={`/${objType}/${id}`} >Dettagli</NavLink></button>
+          
         </div>  
     </div>
     
